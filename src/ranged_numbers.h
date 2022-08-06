03079541f2a61f3fd82dac1a33d690353fdc3465
Joseph Osako <josephosako@gmail.com>
2022-08-06 00:18:16 -0400

    Set up using Doctest to test the template.

--- src/ranged_numbers.h
+++ src/ranged_numbers.h
@@ -2,91 +2,118 @@
 #define _RANGED_NUMBERS_H_
 
 #include <iostream>
+#include <cmath>
 
-enum Flow_Characteristic {WRAP, SATURATE, EXCEPT};
-
-class Underflow_Exception
+namespace Ranged 
 {
+  enum Flow_Characteristic {WRAP, SATURATE, EXCEPT};
 
-};
+  class Underflow_Exception
+  {
 
-class Overflow_Exception
-{
+  };
 
-};
+  class Overflow_Exception
+  {
+
+  };
 
 
-template <typename T, T lower, Flow_Characteristic underflow, T upper, Flow_Characteristic overflow> class Ranged_Numbers
-{
+  template <typename T, T lower, Flow_Characteristic underflow, T upper, Flow_Characteristic overflow> class Ranged_Numbers
+  {
   private:
-   T value;
+    T value;
 
-   void restrict_lower()
-   {
+    void restrict_lower()
+    {
       if (value >= lower)
-         return;
+        return;
       else
-      {
-         switch (underflow)
-         {
+        {
+          switch (underflow)
+            {
             case WRAP:
-               value = upper - value;
-               restrict_lower();
-               break;
+              value = upper - abs(lower - value) + 1;
+              break;
             case SATURATE:
-               value = lower;
-               break;
+              value = lower;
+              break;
             default:
-               throw Underflow_Exception();
-         }
-      }
-   };
+              throw Underflow_Exception();
+            }
+        }
+    };
 
-   void restrict_upper()
-   {
+    void restrict_upper()
+    {
       if (value <= upper)
-         return;
+        return;
       else
-      {
-         switch (overflow)
-         {
+        {
+          switch (overflow)
+            {
             case WRAP:
-               value = lower + value;
-               restrict_upper();
-               break;
+              value = lower + abs(upper - value) - 1;
+              break;
             case SATURATE:
-               value = upper;
-               break;
+              value = upper;
+              break;
             default:
-               throw Overflow_Exception();
-         }
-      }
-   };
+              throw Overflow_Exception();
+            }
+        }
+    };
 
   public:
 
-   Ranged_Numbers(T v):value(v)
-   {
+    Ranged_Numbers(T v):value(v)
+    {
       restrict_lower();
       restrict_upper();
-   };
+    };
 
-   Ranged_Numbers& operator+(Ranged_Numbers& op)
-   {
-      Ranged_Numbers result(this->value + op.value);
-      return result;
-   };
+    Ranged_Numbers operator+(const Ranged_Numbers& op)
+    {
+      return Ranged_Numbers(this->value + op.value);
+    };
 
-  template <typename OST> friend std::ostream& operator<<(std::ostream& os, OST& rn);
+    Ranged_Numbers operator-(const Ranged_Numbers& op)
+    {
+      return Ranged_Numbers(this->value - op.value);
+    };
 
+    Ranged_Numbers operator*(const Ranged_Numbers& op)
+    {
+      return Ranged_Numbers(this->value * op.value);
+    };
+
+    Ranged_Numbers operator/(const Ranged_Numbers& op)
+    {
+      return Ranged_Numbers(this->value / op.value);
+    };
+
+   Ranged_Numbers operator=(const Ranged_Numbers& op)
+    {
+      this->value = op.value;
+    };
+
+
+    bool operator==(const Ranged_Numbers& op)
+    {
+      return (this->value == op.value);
+    };
+
+
+    template <typename OST> friend std::ostream& operator<<(std::ostream& os, const OST& rn);
+
+  };
+
+
+  template <typename OST> std::ostream& operator<<(std::ostream& os, const OST& rn)
+  {
+    os << rn.value;
+    return os;
+  }
 };
-
-
-template <typename OST> std::ostream& operator<<(std::ostream& os, OST& rn)
-{
-  os << rn.value;
-  return os;
-}
-
 
 #endif
--- tests/test_ranged_numerics.cpp
+++ tests/test_ranged_numerics.cpp
@@ -1,14 +1,39 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
+#include "doctest/doctest.h"
 #include "ranged_numbers.h"
 
-typedef Ranged_Numbers<int16_t, -273, SATURATE, 10000, SATURATE> Celsius_Temp;
+typedef Ranged::Ranged_Numbers<int16_t, -273, Ranged::EXCEPT, 10000, Ranged::SATURATE> Celsius_Temp;
+typedef Ranged::Ranged_Numbers<uint16_t, 10, Ranged::WRAP, 25, Ranged::WRAP> Ten_25;
 
-
-int main()
+TEST_CASE("Test a simple range representing Celsius temperatures.")
 {
-  Celsius_Temp a(-300), b(10001);
-  
-  std::cout << a << std::endl;
-  std::cout << b << std::endl;
+  Celsius_Temp x(10000), y(10001);
+  REQUIRE_THROWS(Celsius_Temp(-300));
+  CHECK((x == y));
+  REQUIRE_THROWS(Celsius_Temp(-1) + Celsius_Temp(-273));
+  CHECK((x == (Celsius_Temp(10000) + Celsius_Temp(1))));
 }
 
+
+TEST_CASE("Test wrapping in a simple range of 10 to 25.")
+{
+  std::cout << Ten_25(10) << std::endl;
+  std::cout << Ten_25(25) << std::endl;
+  std::cout << Ten_25(9) << std::endl;
+  std::cout << Ten_25(8) << std::endl;
+  std::cout << Ten_25(7) << std::endl;
+  std::cout << Ten_25(6) << std::endl;
+  std::cout << Ten_25(26) << std::endl;
+  std::cout << Ten_25(27) << std::endl;
+  std::cout << Ten_25(28) << std::endl;
+  std::cout << Ten_25(29) << std::endl;
+  CHECK((Ten_25(25) == (Ten_25(9))));
+  CHECK((Ten_25(24) == (Ten_25(8))));
+  CHECK((Ten_25(23) == (Ten_25(7))));
+  CHECK((Ten_25(22) == (Ten_25(6))));
+  CHECK((Ten_25(10) == (Ten_25(26))));
+  CHECK((Ten_25(11) == (Ten_25(27))));
+  CHECK((Ten_25(12) == (Ten_25(28))));
+  CHECK((Ten_25(13) == (Ten_25(29))));
+}
